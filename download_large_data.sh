4a479fc9b2b9e47e3068c086348768e0b72bdcf9
Xiuqi Pan <xp76@cornell.edu>
2025-04-22 16:51:42 -0400

    Add scripts to download large example files and add large_data file to gitignore

---
+++ download_large_data.sh
@@ -0,0 +1,8 @@
+
+wget \
+    -O large_data/ENCFF775FNU.mn.bw \
+    https://www.encodeproject.org/files/ENCFF775FNU/@@download/ENCFF775FNU.bigWig
+
+wget \
+    -O large_data/ENCFF565BWR.pl.bw \
+    https://www.encodeproject.org/files/ENCFF565BWR/@@download/ENCFF565BWR.bigWig
