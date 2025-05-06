544548541fe6c067c42f6d4012d523d64686252d
Xiuqi Pan <xp76@cornell.edu>
2025-05-06 18:55:42 -0400

    Add skeleton for track2tss_bed

---
+++ track2tss_bed.py
@@ -0,0 +1,20 @@
+
+
+from RGTools.GenomicElements import GenomicElements
+
+class Track2TssBed:
+    @staticmethod
+    def set_parser(parser):
+        pass
+
+    @staticmethod
+    def get_output_site_types():
+        pass
+
+    @staticmethod
+    def get_output_site_coord(region, track, output_site_type):
+        pass
+
+    @staticmethod
+    def main(args):
+        pass
