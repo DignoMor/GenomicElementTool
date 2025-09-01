4a636214e728ada40d31672498782cf2da16e05e
Xiuqi Pan <xp76@cornell.edu>
2025-09-01 17:02:04 -0400

    Add export to CountTable method and corresponding tests

--- export.py
+++ export.py
@@ -1,37 +1,96 @@
 
 from RGTools.GenomicElements import GenomicElements
 
+import pandas as pd
+
 class GenomicElementExport:
     @staticmethod
     def set_parser(parser):
+        subparsers = parser.add_subparsers(dest="oformat")
+
+        parser_exogeneous_sequences = subparsers.add_parser("ExogeneousSequences", 
+                                                          help="Export exogeneous sequences.",
+                                                          )
+        GenomicElementExport.set_parser_exogeneous_sequences(parser_exogeneous_sequences)
+
+        parser_count_table = subparsers.add_parser("CountTable", 
+                                                  help="Export count table.",
+                                                  )
+        GenomicElementExport.set_parser_count_table(parser_count_table)
+
+    @staticmethod
+    def set_parser_exogeneous_sequences(parser):
         GenomicElements.set_parser_genome(parser)
         GenomicElements.set_parser_genomic_element_region(parser)
-        
         parser.add_argument("--oheader", 
                             help="Header of the output file.",
                             required=True,
                             )
+        return parser
 
-        parser.add_argument("--oformat", 
-                            help="Format of the output file.",
+    @staticmethod
+    def set_parser_count_table(parser):
+        GenomicElements.set_parser_genomic_element_region(parser)
+        parser.add_argument("--opath", 
+                            help="Output path of the count table.",
                             required=True,
                             )
 
+        parser.add_argument("--sample_name", 
+                            help="Sample name.",
+                            action="append",
+                            required=True,
+                            )
+        
+        parser.add_argument("--stat_npy", 
+                            help="Path to the stat npy file.",
+                            action="append",
+                            required=True,
+                            )
+        
+        return parser
+    
     @staticmethod
     def get_oformat_options():
-        return ["ExogeneousSequences"]
+        return ["ExogeneousSequences", "CountTable"]
 
     @staticmethod
     def export_exogeneous_sequences(args):
-        ge = GenomicElements(args.region_path, 
+        ge = GenomicElements(args.region_file_path, 
                              args.region_file_type, 
                              args.genome_path, 
                              )
         ge.export_exogeneous_sequences(args.oheader + ".fa")
 
     @staticmethod
+    def export_count_table(args):
+        ge = GenomicElements(args.region_file_path, 
+                             args.region_file_type, 
+                             None, 
+                             )
+        
+        for sample_name, stat_npy in zip(args.sample_name, args.stat_npy):
+            ge.load_region_anno_from_npy(sample_name, stat_npy)
+        
+        region_names = []
+        for region in ge.get_region_bed_table().iter_regions():
+            region_names.append(region["chrom"] + ":" + str(region["start"]) + "-" + str(region["end"]))
+
+        output_df = pd.DataFrame(columns=args.sample_name, 
+                                 index=region_names,
+                                 )
+
+        for sample_name in args.sample_name:
+            output_df[sample_name] = ge.get_anno_arr(sample_name)
+
+        output_df.to_csv(args.opath, 
+                         )
+
+    @staticmethod
     def main(args):
         if args.oformat == "ExogeneousSequences":
             GenomicElementExport.export_exogeneous_sequences(args)
+        elif args.oformat == "CountTable":
+            GenomicElementExport.export_count_table(args)
         else:
             raise ValueError(f"Invalid output format: {args.oformat}")
--- tests/GenomicElementExportTest.py
+++ tests/GenomicElementExportTest.py
@@ -4,6 +4,9 @@ import argparse
 import shutil
 import os
 
+import pandas as pd
+import numpy as np
+
 from export import GenomicElementExport
 
 class GenomicElementExportTest(unittest.TestCase):
@@ -14,6 +17,10 @@ class GenomicElementExportTest(unittest.TestCase):
         if not os.path.exists(self.__wdir):
             os.makedirs(self.__wdir)
         
+        self.__sample1_npy_path = os.path.join(self.__wdir, "sample1.npy")
+        self.__sample2_npy_path = os.path.join(self.__wdir, "sample2.npy")
+        np.save(self.__sample1_npy_path, np.array([1,2,3]))
+        np.save(self.__sample2_npy_path, np.array([4,5,6]))
         super().setUp()
 
     def tearDown(self):
@@ -24,7 +31,7 @@ class GenomicElementExportTest(unittest.TestCase):
 
     def test_export_exogeneous_sequences(self):
         args = argparse.Namespace(
-            region_path=self.__bed3_path,
+            region_file_path=self.__bed3_path,
             region_file_type="bed3",
             genome_path=self.__fasta_path,
             oheader=os.path.join(self.__wdir, "test"),
@@ -37,4 +44,24 @@ class GenomicElementExportTest(unittest.TestCase):
             lines = handle.readlines()
             self.assertEqual(len(lines), 6)
             self.assertEqual(lines[1][:10], "CCCCATCCCC")
+    
+    def test_export_count_table(self):
 
+        args = argparse.Namespace(
+            region_file_path=self.__bed3_path,
+            region_file_type="bed3",
+            opath=os.path.join(self.__wdir, "test.count.csv"),
+            oformat="CountTable",
+            sample_name=["sample1", "sample2"],
+            stat_npy=[self.__sample1_npy_path, self.__sample2_npy_path],
+        )
+        GenomicElementExport.export_count_table(args)
+
+        output_df = pd.read_csv(args.opath, 
+                                index_col=0,
+                                )
+
+        self.assertEqual(output_df.shape, (3, 2))
+        self.assertEqual(output_df.iloc[0, 0], 1)
+        self.assertEqual(output_df.iloc[1, 0], 2)
+        self.assertEqual(output_df.iloc[2, 0], 3)
